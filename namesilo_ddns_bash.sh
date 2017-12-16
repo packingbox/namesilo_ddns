4a310045d85d0d4e0e4724f0f14a930c765f26ed
packingbox <packingbox@163.com>
2017-12-17 02:28:37 +0800

    Update namesilo_ddns_bash.sh

--- namesilo_ddns_bash.sh
+++ namesilo_ddns_bash.sh
@@ -3,13 +3,26 @@
 # change the domain name and api key with yours.
 # get api key from https://www.namesilo.com/account_api.php
 
-domain_name="yours.com"
+domain_name="myds.cloud"
 sub_domain="test"
-api_key="454544323434434"
+api_key="37271a193a9f47170fbe1"
 
 # define domain_name and temp xml file
 domain_xml="/tmp/namesilo_response.xml"
 domain_txt="/tmp/namesilo_response.txt"
+
+
+# check ip status
+# get current ip address
+check_ip=http://api.ipify.org
+current_ip=$(curl -s $check_ip)
+if [[ "$current_ip" = "$(cat $domain_txt)" ]]; then
+    echo "same ip and wait for next check"
+    exit
+else
+    echo $current_ip > $domain_txt 
+    echo "ip changed and start the update"
+fi
 
 ## define variables 
 host_type=""
@@ -46,28 +59,25 @@ while read_dom; do
         
     elif [[ $host_type = "A" ]] && [[ $host_name = "$sub_domain.$domain_name" ]] && [[ $ENTITY = "value" ]]; then
         host_ip="$CONTENT"
-        echo "$host_ip" 
+        echo "$host_rrid $host_type $host_name $host_ip " 
+        break
     fi
 done < $domain_xml
 
-# check ip status
-
-# get current ip address
-check_ip=http://api.ipify.org
-current_ip=$(curl -s $check_ip)
-#echo "$current_ip $host_ip"
-
 # get history ip address
 history_ip=$host_ip
+#echo "$current_ip $history_ip"
 
 # update records
 # https://www.namesilo.com/api_reference.php#dnsUpdateRecord
 dns_update_url="https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=$api_key&domain=$domain_name&rrid=$host_rrid&rrhost=$sub_domain&rrvalue=$current_ip&rrttl=3600"
-#echo $dns_update_url
+echo $dns_update_url
 
 if [[ $current_ip = $history_ip ]]; then
     echo "same ip record, no need to update"
-elif [[ $(curl -s "$dns_update_url" | grep -c "<code>300</code>") > 0 ]]; then
+elif [[ $(curl -s "$dns_update_url" | grep -c "success") > 0 ]]; then
     echo "ip changed and update to $current_ip"
+else
+    echo "update failed"
 fi
 exit 0
